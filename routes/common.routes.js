e0d5dc9c45fe98c4654bc17a3975743826604235
Martynyuk Aleksandr <martinukag@mail.ru>
2025-01-05 16:54:14 +0400

    Added getting log

--- routes/common.routes.js
+++ routes/common.routes.js
@@ -1,10 +1,19 @@
 const { Router } = require('express')
 const router = Router()
 const config = require('config')
+const fs = require('fs')
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename)
 
 
 router.get('/info', (request, response) => {
     response.json({version: config.get('version')})
+})
+
+router.get('/log', async (request, response) => {
+    const today = new Date().toLocaleDateString('RU')
+      
+    response.sendFile(`${appDir}/log/server-${today}.log`)
 })
 
 module.exports = router 
